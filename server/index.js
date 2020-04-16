d66d486432eddb9c142b788556112eb89039a0b8
Eric Liu <eric@mikes-MBP-3.attlocal.net>
2020-04-16 15:27:23 -0700

    Change services endpoints

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
-// const database = require('../db/index.js');
 
 const port = 3000;
 const app = express();
@@ -10,12 +8,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use('/', createProxyMiddleware({ target: 'localhost:3000', changeOrigin: true }))
-app.use('/songData', createProxyMiddleware({ target: 'http://localhost:3001' }))
-app.use('/api/comments', createProxyMiddleware({ target: 'http://localhost:4001' }))
-app.use('/api/track', createProxyMiddleware({ target: 'http://localhost:4001' }))
-app.use('/api/mainSong', createProxyMiddleware({ target: 'http://localhost:3003'}))
-app.use('/songs', createProxyMiddleware({ target: 'http://localhost:3131' }))
+app.use('/api/songs/', createProxyMiddleware({ target: 'http://54.241.202.220' }))
+app.use('/api/comments/songId', createProxyMiddleware({ target: 'http://54.241.202.220' }))
+app.use('/songs', createProxyMiddleware({ target: 'http://3.21.230.11:3000' }))
+app.use('/api/songId', createProxyMiddleware({ target: 'http://54.151.19.84:4001' }))
 
 app.use(express.static('client'));
 
