2a2387e18a60651187e8c666234b2e6d52cf793d
Eric Liu <eric@mikes-MBP-3.attlocal.net>
2020-04-16 20:25:55 -0700

    Proxy bugfixes

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/songs/', createProxyMiddleware({ target: 'http://54.241.202.220' }))
-app.use('/api/comments/songId', createProxyMiddleware({ target: 'http://54.241.202.220' }))
+app.use('/api/songs/', createProxyMiddleware({ target: 'http://54.241.202.220' }));
+app.use('/api/comments/songId', createProxyMiddleware({ target: 'http://54.241.202.220' }));
 app.use('/songs', createProxyMiddleware({ target: 'http://3.21.230.11:3000' }))
 app.use('/api/songId', createProxyMiddleware({ target: 'http://54.151.19.84:4001' }))
 
